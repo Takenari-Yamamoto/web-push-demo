ccbb0f43e00572ef5183c4ff4716e23fac6fe65e
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-04-02 14:36:31 +0900

    edit

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Take!!")
+		fmt.Fprintf(w, "Hello, Take chan!!")
 	})
 
 	http.ListenAndServe(":8080", nil)
