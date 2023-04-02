91c3f25a88a2b9844906e99fc6e02d52d5d5757e
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-04-02 14:42:26 +0900

    yml fix

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Take chan!!")
+		fmt.Fprintf(w, "Hello, Takenari!!")
 	})
 
 	http.ListenAndServe(":8080", nil)
