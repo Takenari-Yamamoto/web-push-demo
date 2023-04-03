baafa9e6b5835a2eb01bd1a94216c52de874f4ec
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-04-03 22:07:47 +0900

    pwa

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -2,12 +2,22 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" manifest="/manifest.json">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta name="description" content="An example of a Next.js PWA" />
+      </Head>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
