9305d5d33f6638c7bc638f0da2a57e0703e004c9
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-04-05 21:06:19 +0900

    feat firebase

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -9,10 +9,10 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#000000" />
-          <meta
+          {/* <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-          />
+          /> */}
           <meta name="description" content="An example of a Next.js PWA" />
         </Head>
         <body>
